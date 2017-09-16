58d008cfb430586e987815c34a30080d59784226
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-16 13:14:54 -0500

    new filter function and test

--- lab-beeksma/lib/fp.js
+++ lab-beeksma/lib/fp.js
@@ -4,3 +4,7 @@
 exports.nMap = (array, operator) => {
   return Array.prototype.map.call(array, operator);
 };
+
+exports.nFilter = (array, selector) => {
+  return Array.prototype.filter.call(array, selector);
+};
--- lab-beeksma/test/fp.test.js
+++ lab-beeksma/test/fp.test.js
@@ -3,8 +3,9 @@
 const { expect } = require('chai');
 const arr = require('../lib/fp.js');
 
-const testArray = [1,2,3];
-const mapResults = [2,4,6];
+const testArray = [1,2,3,4,5,6];
+const mapResults = [2,4,6,8,10,12];
+const filterResults = [2,4,6];
 
 describe('Functions with Call', function(){
   describe('#nMap', function(){
@@ -13,6 +14,17 @@ describe('Functions with Call', function(){
       console.log(newArray);
       expect(newArray).to.be.an('array');
       expect(newArray).to.be.deep.equal(mapResults);
+    });
+  });
+});
+
+describe('Functions with Call', function(){
+  describe('#nFilter', function(){
+    it('should return new array',function(){
+      let newArray = arr.nFilter(testArray,function(x){return (x % 2 === 0);});
+      console.log(newArray);
+      expect(newArray).to.be.an('array');
+      expect(newArray).to.be.deep.equal(filterResults);
     });
   });
 });
